704e1193bea5b60840c281be375d8d76f43eb844
Hashemsroastery <hashem.roestery@gmail.com>
2025-04-30 17:22:57 +0200

    Update products.js

--- src/js/cms-preview-templates/products.js
+++ src/js/cms-preview-templates/products.js
@@ -3,16 +3,16 @@ import Jumbotron from "./components/jumbotron";
 
 export default class ProductsPreview extends React.Component {
   renderProductSection = (section, index, getAsset) => (
-    <div className="mw7 center ph3 pv5" key={index}>
-      <h2 className="f2 b mb4 tc">{section.get("heading")}</h2>
+    <div className="mw7 center ph3 pv3" key={index}>
+      <h2 className="f2 b mb3 tc">{section.get("heading")}</h2>
       <div className="flex flex-wrap mhn1">
         {(section.get("products") || []).map((product, i) => (
-          <div className="w-100 w-33-ns ph2 mb5" key={i}>
+          <div className="w-100 w-33-ns ph2 mb4" key={i}>
             <img
               src={getAsset(product.get("image"))}
               alt=""
-              className="db mb3 center br2 shadow-1"
-              style={{ width: "100%", maxWidth: "340px", height: "auto" }}
+              className="db mb2 center br2 shadow-1"
+              style={{ width: "100%", maxWidth: "450px", height: "auto" }}
             />
             <p className="tc f5 lh-copy">{product.get("description")}</p>
           </div>
